e880879dce318144c8bda2e420bc546d726ab10d
Nick Hall <nickhall122@gmail.com>
2017-11-20 17:14:37 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class EyesOnMe extends React.Component {
+
+    good = () => {
+   console.log('Good!');
+    }
+
+    bad = () => {
+   console.log('Hey! Eyes on me!');
+    }
+
+  render(){
+    return(
+    <button onBlur={this.bad} onFocus={this.good}>wazzup </button>
+  )
+  }
+}
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
 // Code Keypad Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Keypad extends React.Component {
+  // constructor(){
+  //   super();
+  //
+  //
+  //   }
+    passw = () => {
+   console.log('Entering password...');
+    }
+
+  render(){
+    return(
+    <input type="password" onKeyUp={this.passw} />
+  )
+  }
+}
+
+
+export default Keypad;
